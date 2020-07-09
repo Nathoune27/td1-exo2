b3286863c83c11d01d3abc523cf43daa633ccb81
Nathoune27 <68045427+Nathoune27@users.noreply.github.com>
2020-07-09 16:46:05 +1100

    Mise à jour main.blocks, main.py

--- main.py
+++ main.py
@@ -1,1 +1,8 @@
-basic.show_string("bonjour")
+for index in range(2):
+    basic.show_string("bonjour")
+    basic.pause(500)
+    basic.show_number(10853)
+    basic.pause(500)
+    basic.show_arrow(ArrowNames.SOUTH)
+    basic.pause(500)
+    basic.show_icon(IconNames.HAPPY)
